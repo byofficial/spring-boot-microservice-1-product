f1abb7f8166104882df8376873348c17fd021380
Burak <burak.yildiz0@outlook.com>
2022-01-02 20:58:25 +0300

    product service sınıfının oluşturulması

--- src/main/java/com/sha/springbootmicroservice1product/repository/IProductRepository.java
+++ src/main/java/com/sha/springbootmicroservice1product/repository/IProductRepository.java
@@ -3,5 +3,5 @@ package com.sha.springbootmicroservice1product.repository;
 import com.sha.springbootmicroservice1product.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface IProductRepository extends JpaRepository<Product, Long> {
 }
